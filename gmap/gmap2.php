3c68d0a8e68d1591b4f6b256871b7d61855869c6
iman <iman@bitmascot.com>
2018-07-05 13:12:44 +0600

    google map

--- gmap/gmap2.php
+++ gmap/gmap2.php
@@ -8,18 +8,57 @@
 
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript">
-    var geocoder = new google.maps.Geocoder();
-    var address = "new york";
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var latitude = results[0].geometry.location.lat();
-            var longitude = results[0].geometry.location.lng();
-        }
-    });
+//    var geocoder = new google.maps.Geocoder();
+//    var address = "new york";
+//    var addresses = [];
+//    addresses.push('new york');
+//    addresses.push('WebAlive, 910 Yarra Street, South Yarra VIC, Australia');
+//    geocoder.geocode( { 'address': address}, function(results, status) {
+//        if (status == google.maps.GeocoderStatus.OK) {
+//            var latitude = results[0].geometry.location.lat();
+//            var longitude = results[0].geometry.location.lng();
+//        }
+//    });
 </script>
 
+<?php
+
+function getLatituteLongituteByAddress($addresss) {
+    $address = urlencode($addresss);
+    $jsonurl = "http://maps.google.com/maps/api/geocode/json?address=".$address;
+    $json = file_get_contents($jsonurl);
+    $response = json_decode($json, true);
+    if(!empty($response)) {
+        if($response['status'] == 'OK') {
+//            echo '<pre>';
+//            echo print_r($response['results']);
+//            echo '</pre>';
+            return $response['results'][0]['geometry']['location'];
+        }
+    }
+    return '';
+}
+
+$address = 'WebAlive, 910 Yarra Street, South Yarra VIC, Australia';
+$lat_lng = getLatituteLongituteByAddress($address);
+
+echo '<pre>';
+echo print_r($lat_lng);
+echo '</pre>';
+
+?>
+
 <script>
-    function myMap() {
+    function initMap() {
+
+//        var geocoder = new google.maps.Geocoder();
+//        var address = "WebAlive, 910 Yarra Street, South Yarra VIC, Australia";
+//        geocoder.geocode( { 'address': address}, function(results, status) {
+//            if (status == google.maps.GeocoderStatus.OK) {
+//                var latitude = results[0].geometry.location.lat();
+//                var longitude = results[0].geometry.location.lng();
+//            }
+//        });
 
         var mapProp = {
             center: new google.maps.LatLng(-37.837544, 144.992918),
@@ -49,8 +88,8 @@
     }
 </script>
 
-<!--<script-->
-<!--    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAy0AhMvIGk9skz_hhVTeJ1m0CMwaywLGs&callback=myMap"></script>-->
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAy0AhMvIGk9skz_hhVTeJ1m0CMwaywLGs&callback=initMap"></script>
 
 </body>
 </html>
