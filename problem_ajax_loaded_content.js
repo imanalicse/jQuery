ae9762d971cb9eaaea297544fcc3699dc08859a7
iman <iman@bitmascot.com>
2019-10-22 18:04:39 +0600

    site map

--- problem_ajax_loaded_content.js
+++
@@ -1,4 +0,0 @@
-$('.step_1_form').live('submit', function (event) {))
-
-//for loaded content use the following code
-$(document).on('submit','.step_1_form',function(event){})
--- sitemap/js/custom.js
+++ sitemap/js/custom.js
@@ -1,86 +1,231 @@
-  (function($) {
-    $('.tabs .tab-links a').on('click', function(e){
-        e.preventDefault();
-        var currentAttrValue = $(this).attr('href');
-        $('.tabs ' + currentAttrValue).show(500).siblings().hide(500);
-        $(this).parent('li').addClass('active').siblings().removeClass('active');
-    });
+$(document).ready(function () {
+    var $sub_page_selector = $(".arrow-box-b");
+    var $same_level_page_selector = $(".arrow-box-r");
+    var $delete_selector = $(".opt-ic");
 
+    var sub_page_width = $sub_page_selector.outerWidth();
+    var current_page;
+    var page = 1;
 
-    function close_accordion_section(){
-        $('.accordion .accordion-section-title').removeClass('active');
-        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-    }
-    $('.accordion-section-title').click(function(e){
-        e.preventDefault();
+    $(document).on('mouseenter', '.area-block', function (e) {
+        var _self = $(this);
+        var top = _self.offset().top;
+        var left = _self.offset().left;
+        var area_block_height = $('.area-block').outerHeight();
+        var area_block_width = $('.area-block').outerWidth();
+        $sub_page_selector.show();
 
-        var currentAttrValue = $(this).attr('href');
+        $sub_page_selector.css({
+            left: left + (area_block_width / 2) - (sub_page_width / 2),
+            top: top + area_block_height
+        });
 
-        if($(e.target).is('.active')){
-        }else{
-            close_accordion_section();
-
-            $(this).addClass('active');
-
-            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
-        }
-    });
-
-
-
-}(jQuery));
-
-
-jQuery(document).ready(function () {
-    $(document).ready(function () {
-        var $subPage = $(".arrow-box-b");
-
-        var sub_page_width = $(".arrow-box-b").outerWidth();
-        var current_page;
-
-        $(document).on('mouseenter', '.area-block', function (e) {
-            var _self = $(this);
-            var top = _self.offset().top;
-            var left = _self.offset().left;
-            var area_block_height = $('.area-block').outerHeight();
-            var area_block_width = $('.area-block').outerWidth();
-            $subPage.show();
-            $subPage.css({
-                left: left + (area_block_width / 2) - (sub_page_width / 2),
-                top: top + area_block_height
+        if(!_self.hasClass('page-root')) {
+            $same_level_page_selector.show();
+            $same_level_page_selector.css({
+                left: left + area_block_width,
+                top: top
             });
+        }
 
-            current_page = $(this).closest(".tree-leaf");
+        current_page = $(this).closest(".tree-leaf");
 
-        }).on('mouseleave', '.area-block',  function(e){
+    }).on('mouseleave', '.area-block',  function(e){
+        //console.log(e.relatedTarget);
+        if($(e.relatedTarget).hasClass('arrow-box-b')){
+            $(document).on('mouseleave', '.arrow-box-b', function (e) {
+                $sub_page_selector.hide();
+            });
+        }else{
+            $sub_page_selector.hide();
+        }
 
-            if($(e.relatedTarget).hasClass('arrow-box-b')){
-                $(document).on('mouseleave', '.arrow-box-b', function (e) {
-                    $(".arrow-box-b").hide();
+        if($(e.relatedTarget).hasClass('arrow-box-r-inner')){
+            $(document).on('mouseleave', '.arrow-box-r-inner', function (e) {
+                $same_level_page_selector.hide();
+            });
+        }else{
+            $same_level_page_selector.hide();
+        }
+
+    });
+
+    $sub_page_selector.on('click', function () {
+
+        page++;
+
+        var sub_pages = current_page.find('> .tree-children').html();
+
+        var nochilds_class = '';
+        if(!sub_pages || sub_pages.trim().length == 0){
+            nochilds_class = 'nochilds';
+        }
+        current_page.find('> .tree-node').find(".area-block").removeClass('nochilds');
+
+        var subpage_html = '<li class="tree-leaf">'+
+            '<div class="tree-node">'+
+            '<div class="blank-space"></div>'+
+            '<div class="area-block '+nochilds_class+'">'+
+            '<textarea maxlength="250" class="disabled">Page '+page+'</textarea>'+
+            '<div class="opt-ic">'+
+            '<i class="fas fa-ic fa-minus-circle"></i>'+
+            '</div>'+
+            '<div class="cps-ic">'+
+            '<i class="fas fa-ic fa-caret-up"></i>'+
+            '</div>'+
+            '</div>'+
+            '<div class="blank-space"></div>'+
+            '</div>'+
+            '<ul class="tree-children">'+ sub_pages
+        '</ul>'+
+        '</li>';
+
+        current_page.find("> .tree-children").html(subpage_html);
+
+        hideIndicator();
+        domTrigger();
+    });
+
+    $same_level_page_selector.on("click", function () {
+        page++;
+        var same_level_page_template = '<li class="tree-leaf">'+
+            '<div class="tree-node">'+
+            '<div class="blank-space"></div>'+
+            '<div class="area-block nochilds">'+
+            '<textarea maxlength="250" class="disabled">Page '+page+'</textarea>'+
+            '<div class="opt-ic">'+
+            '<i class="fas fa-ic fa-minus-circle"></i>'+
+            '</div>'+
+            '<div class="cps-ic">'+
+            '<i class="fas fa-ic fa-caret-up"></i>'+
+            '</div>'+
+            '</div>'+
+            '<div class="blank-space"></div>'+
+            '</div>'+
+            '<ul class="tree-children">'
+        '</ul>'+
+        '</li>';
+        current_page.after(same_level_page_template);
+        hideIndicator();
+        domTrigger();
+    });
+
+    domTrigger();
+
+
+
+
+    function hideIndicator() {
+        $sub_page_selector.hide();
+        $same_level_page_selector.hide();
+    }
+
+    function domTrigger() {
+
+        $(".area-block").dblclick(function () {
+            var _self = $(this);
+            _self.find("textarea").removeClass("disabled");
+        });
+
+        $(".area-block").on("click", function (event) {
+            var _self = $(this);
+            _self.addClass('sel');
+            _self.find(".opt-ic").show();
+            hideIndicator();
+        });
+
+        $(".opt-ic").on("click", function () {
+            var _self = $(this);
+
+            var removeEl = _self.closest(".tree-leaf");
+            if(removeEl.siblings().length == 0){
+                removeEl.parent().closest(".tree-leaf").find('> .tree-node').find(".area-block").addClass("nochilds");
+            }
+
+            _self.closest(".tree-leaf").remove();
+            hideIndicator();
+        });
+
+
+        $(".tree-leaf").draggable({
+            //cancel: '',
+            //cursor: "crosshair",
+            //distance: 10,
+            //helper: 'clone',
+            revert: 'invalid',
+            //snap: true,
+            opacity: 0.9, // Opacity for the helper while being dragged.
+            zIndex: 100, // Z-index for the helper while being dragged.
+            helper: function () {
+                var _self = $(this);
+                var helperText =  _self.find("> .tree-node").find(".area-block").text();
+                var html = '<div class="drag-box">'+helperText+'</div>';
+                return html;
+            },
+            stop: function( event, ui ) {
+                // console.log('stop');
+                // console.log('event',event);
+                // console.log('ui',ui);
+            }
+
+        });
+
+        $(".area-block").droppable({
+            //snap: true,
+            hoverClass: "highlight",
+            tolerance: "intersect", //default: intersect, touch, pointer
+            drop: function (event, ui) {
+
+                //From Drag: add nochilds class if has no sibling
+                if($(ui.draggable).siblings().not(".drag-box").length == 0) {
+                    $(ui.draggable).parent().closest(".tree-leaf").find('> .tree-node').find(".area-block").addClass('nochilds');
+                }
+
+                // Insert element
+                $(this).closest(".tree-leaf").find("> .tree-children").append(ui.draggable);
+
+                //In drag: Child class management
+                var current_node = $(this).closest(".tree-leaf");
+                if(current_node.length > 0){
+                    current_node.find('> .tree-node').find(".area-block").removeClass('nochilds');
+                }else{
+                    current_node.find('> .tree-node').find(".area-block").addClass('nochilds');
+                }
+                current_node.find(".tree-leaf").each(function () {
+                    var inner = $(this);
+                    if(inner.find(".tree-leaf").length > 0){
+                        inner.find(".tree-leaf").find('> .tree-node').find(".area-block").removeClass('nochilds');
+                    }else{
+                        inner.find(".tree-leaf").find('> .tree-node').find(".area-block").addClass('nochilds');
+                    }
                 });
-            }else{
-                $(".arrow-box-b").hide();
             }
         });
 
-        $subPage.on('click', function () {
-            console.log(current_page);
-            var subpage_Html = '<li class="tree-leaf">'+
-                '<div class="tree-node">'+
-                '<div class="blank-space"></div>'+
-                '<div class="area-block nochilds">'+
-                '<textarea maxlength="250">Page</textarea>'+
-                '</div>'+
-                '<div class="blank-space"></div>'+
-                '</div>'+
-                '<ul class="tree-children">'+
-                '<li class="tree-leaf"></li>'+
-                '</ul>'+
-                '</li>';
-
-            current_page.find(".tree-children").html(subpage_Html);
+        $(".area-block textarea").on("keyup", function () {
+            $(this).html($(this).val());
         });
+    }
+
+    $(document).click(function(e) {
+        if($(e.target).closest('.area-block').length){
+            var element = $(e.target).closest('.area-block');
+            $(".area-block").not(element).removeClass("sel");
+            $(".area-block").not(element).find('.opt-ic').hide();
+        }else{
+            unSelectDeleteElement();
+        }
+
+        if($(e.target).closest('.area-block').find("textarea").length){
+            var element = $(e.target).closest('.area-block').find("textarea");
+            $(".area-block").find("textarea").not(element).addClass("disabled");
+        }else{
+            $(".area-block").find("textarea").addClass("disabled");
+        }
     });
 
+    function unSelectDeleteElement() {
+        $(".opt-ic").hide();
+        $(".area-block").removeClass('sel');
+    }
 });
-
